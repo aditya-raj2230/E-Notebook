93ed1a8cda7917944547440618a48866fc266fbc
aditya-raj2230 <aditya17025@gmail.com>
2024-03-19 08:45:54 +0530

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="text-3xl text-gray-300" ></div>
+      hello
+    </div>
+  );
+}
+
+export default App;
