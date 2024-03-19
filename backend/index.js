8b2e02a9eaf67870efcd01d6451029abec311057
aditya-raj2230 <aditya17025@gmail.com>
2024-03-19 21:38:32 +0530

    database connected

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const express = require('express')
-const app = express();
 const cors = require('cors');
 const connection = require('./db');
+const app = express();
 
 connection();
 
