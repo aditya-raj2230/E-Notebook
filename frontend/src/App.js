99ba4872dad79d83205d40b5d1907616ca0a1834
aditya-raj2230 <aditya17025@gmail.com>
2024-03-21 16:05:45 +0530

    hehe

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 
 import React from "react";
-import CodeEditor from "./components/codeEditor/codeEditor";
+import Home from './pages/Home'
 
 function App() {
   return (
     <div className="App h-screen w-screen bg-white dark:bg-black p-4">
-      <CodeEditor/>
+      <Home/>
     </div>
   );
 }
